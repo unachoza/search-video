0e9c3744d2eef2cdaeba195a0670b9072da3787f
Arianna Choza <unachoza@gmail.com>
2019-03-18 17:37:22 -0400

    mapping video items in video list

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/YouTube'
+import VideoList from './VideoList' 
 
 class App extends Component {
+    state = { videos : [] }
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
-        }).then(response => console.log(response))
+        })
+        this.setState({videos: response.data.items})
     }
     render(){
         return (
         <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
+        <VideoList videos={this.state.videos}/>
         </div>)
     }
 }
