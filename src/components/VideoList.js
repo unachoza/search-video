37c7f4406d398d64929809b4afec4fb7776856ff
Arianna Choza <unachoza@gmail.com>
2019-03-18 17:57:56 -0400

    video item styling

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react'
 import VideoItem from './VideoItem'
 
 const VideoList = ({videos}) => {
-   const renderedList =  videos.map((video) => {
-        return <VideoItem />
+   const renderedList =  videos.map(video => {
+        return <VideoItem video={video}/>
 
     })
 
-    return <div>{renderedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 }
 export default VideoList
